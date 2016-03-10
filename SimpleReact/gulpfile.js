fe647a46dea781d9d483ab834ddc05d28d4658f1
chrisobriensp <chrisobriensp@users.noreply.github.com>
2016-03-10 23:55:49 +0000

    Added files via upload

--- SimpleReact/gulpfile.js
+++ SimpleReact/gulpfile.js
@@ -2,7 +2,22 @@ var gulp = require('gulp');
 var ts = require('gulp-typescript');
 var merge = require('merge2');
 var sass = require('gulp-sass');
+var sourcemaps = require("gulp-sourcemaps");
+var babel = require("gulp-babel");
+var concat = require("gulp-concat");
 
+// starting to integrate jsx/babel as well as typescript..
+gulp.task("scripts", function () {
+  return gulp.src("src/**/*.js")
+    .pipe(sourcemaps.init())
+    .pipe(babel())
+    .pipe(concat("all.js"))
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest("dist"));
+});
+
+/*
+-- just deals with typescript files..
 gulp.task('scripts', function() {
   var tsResult = gulp.src('*.ts')
     .pipe(ts({
@@ -17,6 +32,7 @@ gulp.task('scripts', function() {
     tsResult.js.pipe(gulp.dest('release/js'))
     ]);
 });
+*/
 
 gulp.task('sass', function () {
   gulp.src('*.scss')
