ecf7fddd4bd4d8532a757cdec5b49d301cd86bb6
chrisobriensp <chrisobriensp@users.noreply.github.com>
2016-03-12 15:48:07 +0000

    Delete animal.ts

--- SimpleReact/animal.ts
+++
@@ -1,7 +0,0 @@
-class Animal {
-    name:string;
-    constructor(theName: string) { this.name = theName; }
-    move(meters: number = 0) {
-        alert(this.name + " moved " + meters + "m.");
-    }
-}
