3c123ae3b11dcb33ce32b8cedfbdb60b5f90e27a
chrisobriensp <chrisobriensp@users.noreply.github.com>
2016-03-13 11:38:14 +0000

    Added files via upload

--- SimpleReact/scripts/core.ts
+++ SimpleReact/scripts/core.ts
@@ -1,5 +1,6 @@
 /// <reference path="../release/definitions/main.d.ts" />
 /// <reference path="../release/definitions/jquery.d.ts" />
+/// <reference path="../release/definitions/handlebars.d.ts" />
 
 (function() {
     var animal: Animal = new Animal('zebra');
@@ -8,14 +9,21 @@
     var human: Human = new Human('COB');
     human.callHuman();
 
-
-    jQuery('#container').text('bar');
-    
     var spData = new SPData('someUrl');
     var jsonData : string = spData.GetData(10);
-   // jsonData = `{"test":"test11"}`;
-    var foo = JSON.parse(jsonData);
+    var listItems = JSON.parse(jsonData);
     console.log('Parsed list data..');
+    
+    // simple output with jQuery..
+    jQuery.each(listItems.value, function() {
+        jQuery('#jq-output').append('<div>Item GUID - ' + this.GUID + '</div');
+    })
+
+    // output with handlebars..
+    var source   = $("#listitem-template").html();
+    var template = Handlebars.compile(source);
+    var html = template(listItems);
+    jQuery('#handlebars-output').append(html);
 })();
 
 
